e60ff96a0a5a6a1412e2b95b0af47a538fab30d0
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-03-21 16:46:14 +0100

    updates

---
+++ index.js
@@ -0,0 +1,22 @@
+//create express server, require express library
+//all the librarys I need
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
+dotenv.config();
+//here I connect to my mongoDB, password and DB name
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DBConnection Successful!"))
+  .catch((error) => {
+    console.log(error);
+  });
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
