d707f8cc01eeec8f3a2cfda352b5fb1bfc73e822
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-04-01 14:46:46 +0200

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //Register User
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
   try {
@@ -14,6 +18,31 @@ router.post("/register", async (req, res) => {
     res.status(201).json(savedUser);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+//Create Log in
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    !user && res.status(401).json("Wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    Originalpassword != req.body.password &&
+      res.status(401).json("Wrong credentials!");
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    req.status(500).json(err);
   }
 });
 
