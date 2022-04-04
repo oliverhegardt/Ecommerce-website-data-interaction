f117f2d2577315732ff2e02dfdb9644fc3c73d84
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-04-04 10:25:20 +0200

    updates

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //Register User
 router.post("/register", async (req, res) => {
@@ -31,13 +32,23 @@ router.post("/login", async (req, res) => {
 
     const hashedPassword = CryptoJS.AES.decrypt(
       user.password,
-      process.env.PASS_SEC
+      process.env.PASS_SEC,
+      { expiresIn: "3d" }
     );
 
     const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
     Originalpassword != req.body.password &&
       res.status(401).json("Wrong credentials!");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC
+    );
+
     const { password, ...others } = user._doc;
 
     res.status(200).json(others);
