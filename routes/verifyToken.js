5839c2248fe19602b1fc486db1abd511b050e580
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-10-03 09:56:32 +0200

    Update verifyToken.js

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -5,16 +5,17 @@ const jwt = require("jsonwebtoken");
 //How am I going to verfiy my webtoken?
 
 const verfiyToken = (req, res, next) => {
-  const authHeader = req.headers.token;
-  if (authHeader) {
-    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) res.status(403).json("Token is not valid!");
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.status(401).json("You are not authenticated");
-  }
+    const authHeader = req.headers.token;
+    if (authHeader) {
+        jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+            if (err) res.status(403).json("Token is not valid!");
+            //användaren är verifierad
+            req.user = user;
+            next();
+        });
+    } else {
+        return res.status(401).json("You are not authenticated");
+    }
 };
 
 module.exports = { verfiyToken };
